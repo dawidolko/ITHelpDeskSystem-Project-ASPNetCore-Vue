109d3ad0153da4a8f8d5754424a6210334c460d4
dawidolko <do125148@stud.ur.edu.pl>
2026-02-14 00:08:18 +0100

    Fix: Docker files

--- backend/Program.cs
+++ backend/Program.cs
@@ -48,7 +48,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowVueApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000")
+            policy.WithOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://localhost:8080")
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials();
